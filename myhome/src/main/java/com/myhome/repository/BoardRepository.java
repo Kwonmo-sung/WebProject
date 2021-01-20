621e9e83dff870deab1e1c8f7fa1bdcb285ebed1
kwonmo <rnjsah9@gmail.com>
2021-01-20 23:20:56 +0900

    2021-01-20 jpa를 이용해 게시판 조회하기

---
+++ myhome/src/main/java/com/myhome/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.myhome.repository;
+
+import com.myhome.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long>{
+}
